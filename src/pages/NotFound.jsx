0859ad2e3f8b80bf315a702896cb9756b913e6cf
jay9949 <jay37192@gmail.com>
2023-12-11 17:01:44 +0530

    end

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import pageNotFound from "../assets/all-images/PageNotFound.jpg";
+import "../styles/custom.css";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <img src={pageNotFound} alt="not-found" className="page" />
+    </>
+  );
 };
 
 export default NotFound;
